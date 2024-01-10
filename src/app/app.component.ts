b22dc384b34494e071f8e535ec6176df15315a80
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-01-10 14:45:53 +0400

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { WishItem } from '../shared/models/wishitem';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  items = [
+    new WishItem('to learn'),
+    new WishItem('get', true),
+    new WishItem('find some grass'),
+  ];
   title = 'wishlist-app';
 }
